a785887b8393acad86815cc899693ebfcf29bb89
KristianaKGrigoriadis <68119886+KristianaKGrigoriadis@users.noreply.github.com>
2020-08-17 12:53:34 +0100

    Update loss_functions.py

--- sem_seg/loss_functions.py
+++ sem_seg/loss_functions.py
@@ -1,7 +1,5 @@
-import numpy as np
 import torch
 import torch.nn as nn
-from utils import softmax
 
 
 class DiceLoss(nn.Module):
@@ -13,40 +11,47 @@ class DiceLoss(nn.Module):
         # https://github.com/kevinzakka/pytorch-goodies/blob/master/losses.py
         num_classes = model_output.shape[1]
         score = model_output.clone()
-        score = softmax(score)
-        # print("target.unique() =",target.unique())
+        #score = softmax(score)
+        probas = score.clone()
+        #print("torch.sum(torch.isnan(probas)) =",torch.sum(torch.isnan(probas)))
         target = target.type(torch.LongTensor)
         target_1_hot = torch.eye(num_classes)[target.squeeze(1)]
 
-        probas = score.clone()
-        target_1_hot = target_1_hot.type(score.type())
+        target_1_hot = target_1_hot.type(probas.type())        
         target_1_hot = target_1_hot.reshape(probas.shape)
-        
+
         dims = (0,) + tuple(range(2, target.ndimension()))
         intersection = torch.sum(probas * target_1_hot, dims)
         cardinality = torch.sum(probas + target_1_hot, dims)
+
         dice_loss = (2. * intersection / (cardinality + self.eps)).mean()
         return (1 - dice_loss)
     
+    
 class GeneralisedDiceLoss(nn.Module):
     def __init__(self, eps=1e-7):
-        super(DiceLoss, self).__init__()
+        super(GeneralisedDiceLoss, self).__init__()
         self.eps = eps
     
     def forward(self, model_output, target):
         num_classes = model_output.shape[1]
         score = model_output.clone()        
-        score = softmax(score)
+        #score = softmax(score)
+        probas = score.clone()
 
         target = target.type(torch.LongTensor)
         target_1_hot = torch.eye(num_classes)[target.squeeze(1)]
 
-        probas = score.clone()
         target_1_hot = target_1_hot.type(score.type())
+        target_1_hot = target_1_hot.reshape(probas.shape)
+
         dims = tuple(range(2, target.ndimension()))
         intersection = torch.sum(probas * target_1_hot, dims)
-        weights = 1./((probas**2).sum(dim=dims))
         cardinality = torch.sum(probas + target_1_hot, dims)
+        weights = 1./((probas**2).sum(dim=dims))
+        
         dice_loss = (2. * torch.sum(weights * intersection))/(torch.sum(weights*cardinality) + self.eps)
         return (1 - dice_loss)
+
+
 
