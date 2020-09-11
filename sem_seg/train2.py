12f3ecaf51115de9667ca8b28d03f46abf55b55b
KristianaKGrigoriadis <68119886+KristianaKGrigoriadis@users.noreply.github.com>
2020-09-11 17:35:09 +0200

    Update train2.py

--- sem_seg/train2.py
+++ sem_seg/train2.py
@@ -8,21 +8,13 @@ import torch.nn as nn
 import utils
 
 from torch.utils.data import DataLoader
-# from torchsummary import summary
 from unet import UNet
 
 
-""" To run:
-python3 train2.py -l 'dirpath: /home/kgrigori/patch_data/512/, learningrate: 0.01, batch_size: 1, max_epochs: 100, channels_out: 6, loss: dice'"""
-
-# define a function to train the model
 def train_fn(train_data, model, criterion, optimizer, device):
-    print('Training one epoch.')
-    # save the performance on training data
     train_loss = 0
     train_acc = 0
     for i, (images, labels) in enumerate(train_data):
-        # print('Batch number {}'.format(i))
         optimizer.zero_grad()
         images, labels = images.to(device), labels.to(device)
         output = model(images)
@@ -31,12 +23,10 @@ def train_fn(train_data, model, criterion, optimizer, device):
         loss.backward()
         optimizer.step()
         train_acc += utils.accuracy(output, labels)
-        #print('Batch number {}, \t loss = {}'.format(i, loss.item()))
     return train_loss/len(train_data) , train_acc / len(train_data.dataset)
 
-# define a function to evaluate model performance
-def test_fn(test_data, model, criterion, device):
-    print('Evaluating model on validation data')    
+
+def test_fn(test_data, model, criterion, device):   
     test_loss = 0
     test_acc = 0
     for i, (images,labels) in enumerate(test_data):
@@ -51,10 +41,10 @@ def test_fn(test_data, model, criterion, device):
 
 
 def run_training(train_dl, val_dl, model, loss, optimizer, 
-                 batch_size, patch_size, curtime, device, max_epochs, save=True):
+                 batch_size, patch_size, filename, device, max_epochs, save=True):
     print('\nTraining the model.')
     
-    modelsave = './patch_size_'+ str(patch_size) + '_batch_size_'+str(batch_size)+'_best_model.tar'
+    modelsave = './patch_size_'+ filename[6:] + '_batch_size_'+str(batch_size)+'_best_model.tar'
     
     losses_train = []
     accs_train = []
@@ -69,7 +59,7 @@ def run_training(train_dl, val_dl, model, loss, optimizer,
         print('Epoch: %d' %(epoch))
 
         train_loss, train_acc = train_fn(train_dl, model, loss, optimizer, device)
-        val_loss, val_acc = test_fn(val_dl, model, loss)
+        val_loss, val_acc = test_fn(val_dl, model, loss, device)
         
         losses_train.append(train_loss)
         accs_train.append(train_acc)
@@ -85,25 +75,30 @@ def run_training(train_dl, val_dl, model, loss, optimizer,
                 torch.save({'epoch': epoch,
                             'model_state_dict': model.state_dict(),
                             'optimizer_state_dict': optimizer.state_dict(),
-                            'best_accuracy': best_val_acc,
-                            'batch_size': batch_size,
-                            'best_learning_rate': optimizer.defaults['lr']
-                            },modelsave)
+                            'val_accuracy': val_acc,
+                            'val_loss': val_loss,
+                            'batch_size': batch_size},modelsave)
         
     
-        # if epoch % 10 == 0:
         print(f'\t Train: \tLoss: {train_loss:.6f}\t|\tAcc: {train_acc * 100:.3f}%(train)')
         print(f'\t Validation: \tLoss: {val_loss:.6f}\t|\tAcc: {val_acc * 100:.3f}%(valid)','\n')
                 
-        trainfile = open('./training_check_file_'+patch_size+'_' + curtime,'a')
+        trainfile = open(filename,'a')
         trainfile.write('\nFinished training epoch '+str(epoch)+'\n')
         trainfile.write(f'\t Train: \tLoss: {train_loss:.6f}\t|\tAcc: {train_acc * 100:.3f}%(train)\n')
         trainfile.write(f'\t Validation: \tLoss: {val_loss:.6f}\t|\tAcc: {val_acc * 100:.3f}%(valid)\n')
         trainfile.close()
+        
+        # save loss and acc performance:
+        np.save(filename+'_losses_train', np.array(losses_train))
+        np.save(filename+'_losses_val', np.array(losses_val))
+        np.save(filename+'_accs_train', np.array(accs_train))
+        np.save(filename+'_accs_val', np.array(accs_val))    
 
         # convergence criterion
-        if epoch >= 50 and all((val_loss - torch.tensor(losses_val[-20:-1])) > 0.0):
+        if epoch >= 30 and all((val_loss - torch.tensor(losses_val[-10])) > 0.0):
             break
+        
         
     secs = int(time.time() - start_time)
     mins = secs / 60
@@ -130,10 +125,13 @@ def run_training(train_dl, val_dl, model, loss, optimizer,
     best_val_accuracy = max(accs_val)
     print('Best validation accuracy:',best_val_accuracy*100,'achieved at epoch:',best_val_epoch,'\n')
 
+    
 
 def main():
     parser = argparse.ArgumentParser(description='Train patches')
     parser.add_argument('-l', '--list', help='delimited list input', type=str)
+    parser.add_argument('-w', '--weight', nargs = '+', help='list of class weights',type=float)
+
     args = parser.parse_args()
     argu_list = [[it for it in item.split(': ')] for item in args.list.split(', ')]
     dirpath = argu_list[0][1]
@@ -142,11 +140,21 @@ def main():
     max_epochs = int(argu_list[3][1])
     channels_out = int(argu_list[4][1])
     loss = argu_list[5][1]
+    foreground = argu_list[6][1]
+    
+    print("\nClass weights =",args.weight)
     
     # get datasets
     patch_size = dirpath.split('/')[-2]
-    train_odgt = dirpath + 'split/train/'+patch_size+'_train_patches.odgt'
+    file_start =  dirpath + 'split/train/'+patch_size+'_train_patches'
+    if foreground == 'n':
+        train_odgt = file_start + '.odgt'
+    elif foreground == 'y':
+        train_odgt = file_start + '_foreground.odgt'
+        
     val_odgt = dirpath + 'split/val/'+patch_size+'_val_patches.odgt'
+    
+    print(train_odgt,val_odgt)
 
     
     train_ds = segmentation_dataset.SegmentationDataset(train_odgt,train=True)
@@ -167,33 +175,48 @@ def main():
     # initialising the model
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = UNet(out_ch=channels_out)
+    
+    
     if torch.cuda.device_count()>1:
-        print("Using", torch.cuda.device_count(), "GPUs")
         model = nn.DataParallel(model)
+    print("Using", torch.cuda.device_count(), "GPUs")
     
     curtime = ('_').join(np.array(time.localtime()[:5],dtype=str))
     
-    trainfile = open('./training_check_file_'+str(patch_size) + '_' + curtime,'w')
+    filename = './tcf_'+str(patch_size) + '_' + curtime + loss
+    trainfile = open(filename,'w')
     trainfile.write('Patch size = '+str(patch_size))
+    trainfile.write('\nLoss: '+loss)
+    if args.weight:
+        trainfile.write("\nLoss weights: "+str(args.weight))
+    trainfile.write('\nDirectory: '+dirpath.split('/')[-3])
     trainfile.write("\nUsing "+str(torch.cuda.device_count())+ " GPUs\n")
     trainfile.close()
     model.to(device)
-    # print("\nModel summary: ",summary(model,input_size=(3, 512,512)))
     
     # define the loss function
     if loss == 'dice':
         criterion = loss_functions.DiceLoss().to(device)
     elif loss == 'generalised_dice':
         criterion = loss_functions.GeneralisedDiceLoss().to(device)
+    elif loss == 'cross_entropy_loss':
+        if args.weight:
+            loss_weight = torch.tensor(args.weight).to(device)
+            criterion = nn.CrossEntropyLoss(weight=loss_weight)
+        else:
+            criterion = nn.CrossEntropyLoss()
+    elif loss == 'jaccard':
+        criterion = loss_functions.JaccLoss().to(device)
+
     else:
         print('not a type of loss')
-        assert loss == 'dice'
+        assert (loss in ['cross_entropy_loss','dice','generalised_dice','jaccard'])
 
     # define the optimizer
     optimizer = torch.optim.Adam(model.parameters(), lr=learningrate)
     
     run_training(train_dl, val_dl, model, criterion, optimizer,
-                 batch_size, patch_size, curtime, device, max_epochs, save=True)
+                  batch_size, patch_size, filename, device, max_epochs, save=True)
     
 if __name__=="__main__":
     main()
